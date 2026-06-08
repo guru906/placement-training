226d74ec5825029a1e1ed03e4bdd9b39530c1650
guru906 <guruprasanth014@gmail.com>
2026-06-08 16:52:16 +0530

    Day01 - Arithmetic operations

---
+++ src/DAY_01/arithmetic.java
@@ -0,0 +1,34 @@
+package DAY_01;
+
+import java.util.Scanner;
+
+public class arithmetic {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        //addition
+        System.out.println("Addition : " + (a + b));
+        System.out.println("Subraction : " + (a - b));
+        System.out.println("Multiplication : " + (a * b));
+        System.out.println("Division : " + (a / b));
+        //Area and perimeter of triangle
+        System.out.println("_____________TRIANGLE ___________");
+        int bR = scan.nextInt();
+        int h = scan.nextInt();
+        int s1 = scan.nextInt();
+        int s2 = scan.nextInt();
+        int s3 = scan.nextInt();
+        int perimeter = s1 + s2 + s3;
+
+        System.out.println("PERIMETER OF TRIANGLE : " + perimeter);
+
+        System.out.println("AREA OF TRIANGLE : " + (0.5 * bR * h));
+        System.out.println("_________________RECTANGLE__________________");
+        int L = scan.nextInt();
+        int B = scan.nextInt();
+        System.out.println("PERIMETER OF RECTANGLE : " + (2 * (L + B)));
+
+        System.out.println("AREA OF RECTANGLE  : " + (L * B));
+    }
+}
