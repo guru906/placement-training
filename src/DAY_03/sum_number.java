34489924d6ea80847c9dd5272446e9176522c5af
guru906 <guruprasanth014@gmail.com>
2026-06-10 14:55:39 +0530

    Day03 - Sum Of Number In Given Range

---
+++ src/DAY_03/sum_number.java
@@ -0,0 +1,13 @@
+package DAY_03;
+
+public class sum_number {
+    public static void main(String args[])
+    {
+        int sum=0;
+        for(int i=0;i<=20;i++)
+        {
+            sum =sum+i;
+        }
+        System.out.println("sum of the number " +sum);
+    }
+}
