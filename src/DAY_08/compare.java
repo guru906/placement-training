8a4531760d4db6e149ed41d7e2c30d07b83a7569
guru906 <guruprasanth014@gmail.com>
2026-06-16 16:06:09 +0530

    Day08 - compare two string

---
+++ src/DAY_08/compare.java
@@ -0,0 +1,16 @@
+package DAY_08;
+
+public class compare {
+    public static void main(String args[])
+    {
+        String s="java";
+        String s2="java";
+        String s3=new String("python");
+        String s4=new String("python");
+        System.out.println(s==s2);
+        System.out.println(s.equals(s2));
+        System.out.println(s3==s4);
+        System.out.println(s3.equals(s4));
+
+    }
+}
