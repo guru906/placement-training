970da325c906fcb065d2c3096e43f2abfee92468
guru906 <guruprasanth014@gmail.com>
2026-06-20 16:30:54 +0530

    Day13 - STRING PRACTISE

--- src/DAY_13/checkAnagram.java
+++ src/DAY_13/checkAnagram.java
@@ -3,6 +3,21 @@ import java.util.*;
 public class checkAnagram {
     public static void main(String args[])
     {
+       String str1="CAR";
+       String str2="RAC";
+       char ch[]=str1.toCharArray();
+       char ch1[]=str2.toCharArray();
+       Arrays.sort(ch);
+       Arrays.sort(ch1);
+       String s1=new String(ch);
+       String s2=new String(ch1);
+       if(s1.equals(s2))
+       {
+           System.out.println("GIVEN IS ANAGRAM ");
+       }
+       else{
+           System.out.println("NOT A ANAGRAM ");
+       }
 
     }
 }
