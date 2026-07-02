2fb8b2fb0151d22679e9ee6320e96f29eb2d4460
guru906 <guruprasanth014@gmail.com>
2026-07-02 14:27:00 +0530

    DAY_19 - leetcode

---
+++ src/DAY_19/MINIMUMSUBSTRINGLENGTH.java
@@ -0,0 +1,9 @@
+package DAY_19;
+
+public class MINIMUMSUBSTRINGLENGTH {
+    public static void main(String args[])
+    {
+        int arr[]={2};
+    }
+
+}
---
+++ src/DAY_19/reverseString.java
@@ -0,0 +1,21 @@
+package DAY_19;
+import java.util.*;
+public class reverseString {
+    public static void main(String args[])
+    {
+        String s="hello";
+        char ch[]=s.toCharArray();
+        int start=0;
+        int end=ch.length-1;
+        while(start<=end)
+        {
+            char temp=ch[start];
+            ch[start]=ch[end];
+            ch[end]=temp;
+            start++;
+            end--;
+        }
+        System.out.println(Arrays.toString(ch));
+
+    }
+}
